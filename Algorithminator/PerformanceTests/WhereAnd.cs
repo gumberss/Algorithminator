1fa4a9fe6ac02dc7e4ae6a690b85992701e0bce5
gumberss <gustavomarinsuppi@gmail.com>
2021-10-13 22:26:33 -0300

    chore: check performance of multi wheres and and operator

---
+++ Algorithminator/PerformanceTests/WhereAnd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithminator.PerformanceTests
+{
+    public class WhereAnd
+    {
+        //Slow  
+        public int[] MultiWhere(int[] array)
+        {
+            return array
+                .Where(x => x > 1)
+                .Where(x => x > 2)
+                .Where(x => x > 3)
+                .Where(x => x > 4)
+                .Where(x => x > 5)
+                .Where(x => x > 6)
+                .ToArray();
+        }
+   
+        //Fast
+        public int[] And(int[] array)
+        {
+            return array
+                .Where(x => x > 1 
+                         && x > 2
+                         && x > 3
+                         && x > 4
+                         && x > 5
+                         && x > 6)
+                .ToArray();
+        }
+    }
+}
--- Algorithminator/Program.cs
+++ Algorithminator/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Algorithminator.Fibonacci;
+using Algorithminator.PerformanceTests;
 using BenchmarkDotNet.Running;
 using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Algorithminator
 {
@@ -10,7 +13,34 @@ namespace Algorithminator
         {
             //Console.WriteLine(new FibonacciService().Slow(10));
             //Console.WriteLine(new FibonacciService().LogN(10));
-            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+            //var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+
+            var service = new WhereAnd();
+
+            var array = Enumerable.Range(0, 1_000).ToArray();
+
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            for (int i = 0; i < 1_000_00; i++)
+            {
+                service.MultiWhere(array);
+            }
+            s.Stop();
+
+            Console.WriteLine(s.ElapsedMilliseconds);
+
+            s.Reset();
+            s.Start();
+
+            for (int i = 0; i < 1_000_00; i++)
+            {
+                service.And(array);
+            }
+            s.Stop();
+
+            Console.WriteLine(s.ElapsedMilliseconds);
         }
     }
 }
