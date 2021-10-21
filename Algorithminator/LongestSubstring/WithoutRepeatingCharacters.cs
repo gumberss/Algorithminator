f97468b841400adedd6c1022bc9b6bf81393ff27
gumberss <gustavomarinsuppi@gmail.com>
2021-10-20 21:41:47 -0300

    chore: first string repeating characters implementation

---
+++ Algorithminator/LongestSubstring/WithoutRepeatingCharacters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithminator.LongestSubstring
+{
+    public class WithoutRepeatingCharacters
+    {
+
+        public string From(string text)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            Dictionary<char, int> dic2 = new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dic.ContainsKey(text[i]))
+                    dic.Add(text[i], i);
+                else
+                {
+                    if (dic2.Count < dic.Count)
+                    {
+                        dic2 = new Dictionary<char, int>(dic);
+                    }
+
+                    dic.Clear();
+                }
+            }
+
+            return new string(dic2.OrderBy(x => x.Value).Select(x => x.Key).ToArray());
+        }
+
+        //public string From(string text)
+        //{
+        //    Dictionary<char, int> dic = new Dictionary<char, int>();
+        //    Dictionary<char, int> dic2 = new Dictionary<char, int>();
+
+        //    for (int i = 0; i < text.Length; i++)
+        //    {
+        //        if (!dic.ContainsKey(text[i]))
+        //            dic.Add(text[i], i);
+        //        else
+        //        {
+        //            if (dic2.Count < dic.Count)
+        //            {
+        //                dic2 = new Dictionary<char, int>(dic);
+        //            }
+
+        //            dic.Clear();
+        //        }
+        //    }
+
+        //    return new string(dic2.OrderBy(x => x.Value).Select(x => x.Key).ToArray());
+        //}
+    }
+}
---
+++ AlgorithminatorTests/LongestSubstring/WithoutRepeatingCharactersTests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithminator.LongestSubstring;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace AlgorithminatorTests.LongestSubstring
+{
+    
+    public class WithoutRepeatingCharactersTests
+    {
+        private readonly WithoutRepeatingCharacters _service;
+
+        public WithoutRepeatingCharactersTests()
+        {
+            _service = new WithoutRepeatingCharacters();
+        }
+
+        [Theory]
+        [InlineData("abcabcbb", "abc")]
+        [InlineData("zzzabcdzzz", "zabcd")]
+        public void Should_return_the_correct_sequence(String text, string expected)
+        {
+            var result = _service.From(text);
+
+            result.ShouldBe(expected);
+        }
+    }
+}
