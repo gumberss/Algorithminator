c01c65a98e8894792636311b90a23f11278690bb
gumberss <gustavomarinsuppi@gmail.com>
2023-08-13 17:23:56 -0300

    feat: adding deletion

--- Conflict-Free/ConflictFree/Conflict.cs
+++ Conflict-Free/ConflictFree/Conflict.cs
@@ -8,7 +8,7 @@ namespace ConflictFree
 {
     public class Character
     {
-        public Character(string key, string val,  string? nearCharacterKey)
+        public Character(string key, string val, string? nearCharacterKey)
         {
             Key = key;
             Val = val;
@@ -18,17 +18,25 @@ namespace ConflictFree
         public String Key { get; set; }
         public String Val { get; set; }
         public String? NearCharacterKey { get; set; }
+        public bool Deleted { get; set; }
 
     }
 
     public class Conflict
     {
+        public static List<Character> ProcessDeletion(List<Character> current, Character deleteVal)
+        {
+            current.Find(x => x.Key == deleteVal.Key).Deleted = true;
+
+            return current;
+        }
+
         public static List<Character> ProcessConflict(List<Character> current, Character newVal)
         {
             var near = current.Find(x => x.Key == newVal.NearCharacterKey);
             var nearIndex = current.FindIndex(x => x.Key == newVal.NearCharacterKey);
 
-           var index = FindCorrectIndex(current, newVal, nearIndex);
+            var index = FindCorrectIndex(current, newVal, nearIndex);
 
             current.Insert(index + 1, newVal);
 
@@ -40,8 +48,8 @@ namespace ConflictFree
             if (index + 1 < current.Count)
             {
                 var comparisonResult = string.Compare(current[index + 1].Key, newVal.Key, comparisonType: StringComparison.OrdinalIgnoreCase);
-                if (comparisonResult > 0)
-                    return FindCorrectIndex(current, newVal, index+1);
+                if (comparisonResult > 0 || current[index + 1].Deleted)
+                    return FindCorrectIndex(current, newVal, index + 1);
             }
 
             return index;
